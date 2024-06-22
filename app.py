cfcc5b2772cd8561e34d06cd4bf88c2d197dbbec
vikrant <virat845712@gmail.com>
2024-06-22 19:55:01 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from cal_fun import do_addition, do_subtraction
+
+def main():
+    print('Welcome to the Calculator app')
+    print("""
+Select the function from the given options:
+1. Add
+2. Subtract
+    """)
+
+    try:
+        user_input = input("Select the function: ")
+        print(f"User selected option: {user_input}")
+        
+        a = int(input("Value of A: "))
+        print(f"Value of A: {a}")
+        b = int(input("Value of B: "))
+        print(f"Value of B: {b}")
+      
+        if user_input == "1":
+            result = do_addition(a, b)
+        elif user_input == "2":
+            result = do_subtraction(a, b)
+        else:
+            print("Invalid selection")
+            return
+
+        print('Result:', result)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+if __name__ == "__main__":
+    main()
---
+++ cal_fun.py
@@ -0,0 +1,5 @@
+def do_addition(a: int, b: int) -> int:
+    return a + b
+
+def do_subtraction(a: int, b: int) -> int:
+    return a - b
